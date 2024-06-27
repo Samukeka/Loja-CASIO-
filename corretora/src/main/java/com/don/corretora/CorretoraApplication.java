762d1c7e52ee672f7c173d9721e46ef51cd2026e
Letícia <iambragalets@gmail.com>
2024-06-27 17:44:01 -0300

    feito algumas partes

--- corretora/src/main/java/com/don/corretora/CorretoraApplication.java
+++ corretora/src/main/java/com/don/corretora/CorretoraApplication.java
@@ -2,8 +2,12 @@ package com.don.corretora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EntityScan(basePackages = "com.don.corretora")
 public class CorretoraApplication {
 
 	public static void main(String[] args) {
