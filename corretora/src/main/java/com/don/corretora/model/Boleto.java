03e50a312141428d2f250ee1429ead6fa8440986
Letícia <iambragalets@gmail.com>
2024-07-18 19:17:09 -0300

    finalizado

--- corretora/src/main/java/com/don/corretora/model/Boleto.java
+++ corretora/src/main/java/com/don/corretora/model/Boleto.java
@@ -1,17 +1,18 @@
 package com.don.corretora.model;
 
-import jakarta.persistence.Entity;
+import org.apache.commons.validator.Form;
+
+import jakarta.persistence.*;
 
 @Entity
-public class Boleto implements FormaDePagamento {
+public class Boleto extends FormaDePagamento {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String codigo_de_barras;
 
 
-    @Override
-    public void pagar(){
-
-    }
 
     public String getCodigo_de_barras() {
         return codigo_de_barras;
